bfb434f583e7d9bb0e5f99a3c459ccdba540cbb3
Tawan Fonseca <82100530+Twfonseca@users.noreply.github.com>
2022-02-26 22:32:38 +0044

    Product page started

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ let nameClient= document.querySelector("#nameUser")
 let emailClient= document.querySelector("#emailUser")
 let phoneClient= document.querySelector("#phoneUser")
 let popUp= document.querySelector("#windowJsTarget")
+let container= document.querySelector("#containerTarget")
 
 
 formAction.addEventListener("submit",e =>{ e.preventDefault()})
@@ -11,5 +12,6 @@ formAction.addEventListener("submit",e =>{ e.preventDefault()})
 function moreInfo(){
     if(nameClient.value !=="" && emailClient.value!=="" && phoneClient.value!==""){
     popUp.style ="display: initial;"
+    container.style ="display:none;"
     } 
 }
