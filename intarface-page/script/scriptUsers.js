78e56a01ebb2b1ffb763e6c568265fdb7f2becab
Tawan Fonseca <82100530+Twfonseca@users.noreply.github.com>
2022-03-08 20:29:45 +0044

    script
    some ajust in the script to check login

--- intarface-page/script/scriptUsers.js
+++ intarface-page/script/scriptUsers.js
@@ -7,22 +7,38 @@ var formAction= document.querySelector("#formLogin")
 var login= document.querySelector("#loginBtn")
 
 
-
-
-
 function loginCheck(){
- let logUser= loginUser.value;
- let pass =passUser.value;
- let hierarchy= profileType.value;
+  let logUser= loginUser.value;
+  let pass= passUser.value;
+  let hierarchy= profileType.value;
 
- if(hierarchy=== deanInfo.hierarchy  &&logUser=== deanInfo.loginEmail && pass=== deanInfo.pass){
 
+  if(hierarchy=== deanInfo.hierarchy  &&logUser=== deanInfo.loginEmail && pass=== deanInfo.pass){
      console.log("bateu o login e a senha")
- }else if(hierarchy=== deanInfo.hierarchy  &&logUser=== deanInfo.loginEmail && pass=== deanInfo.pass){
-     
- }
+   } else if (hierarchy === "professor"){
+       professorsCheck(hierarchy,logUser,pass)
+       if(logUser=== loginUser.value && pass=== passUser.value && hierarchy=== profileType.value){
+        console.log("bateu o login e a senha")
+       }
+   }
 
-}
+  }
+
+  function professorsCheck(hierarc,log, pass ){
+      professors.forEach(professor => {
+          if( hierarc=== professor.hierarchy && log=== professor.loginEmail && pass=== professor.pass  ){
+             return hierarc,log, pass
+          }
+      });
+  }
 
 
 
+/* Create a function to check each professor  and their informations in the array professor*/
+
+
+/*else if(hierarchy=== professorsData.hierarchy  && logUser=== professorsData.loginEmail && pass=== professorsData.pass){
+    console.log("bateu o login e a senha")
+ }else if(hierarchy=== studentsData.hierarchy  && logUser=== studentsData.stundentCode && pass=== studentsData.pass){
+    console.log("bateu o login e a senha")
+ } */
